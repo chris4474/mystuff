6f0addc5c0b580fd97187b715d21a1cbfffe96b2
chris4474 <c237537@yahoo.fr>
2022-01-20 14:29:51 +0000

    put the server certificate first in the chain of certificates

--- create-cert.sh
+++ create-cert.sh
@@ -10,8 +10,8 @@ tmpdir=$(mktemp -d)
 vault write -format=json intca/issue/symphorines common_name=${node} alt_names=${node} >${tmpdir}/${node}.json
 
 cat ${tmpdir}/${node}.json | jq -r .data.private_key  >${tmpdir}/key.pem
-cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0]  >${tmpdir}/cert-plus-intca.pem
-cat ${tmpdir}/${node}.json | jq -r .data.certificate >>${tmpdir}/cert-plus-intca.pem
+cat ${tmpdir}/${node}.json | jq -r .data.certificate  >${tmpdir}/cert-plus-intca.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0] >>${tmpdir}/cert-plus-intca.pem
 cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[1]  >${tmpdir}/ca.crt
 cat ${tmpdir}/${node}.json | jq -r .data.certificate  >${tmpdir}/cert.pem
 
