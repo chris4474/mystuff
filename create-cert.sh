e9d330d46ce8bae370b2f11d8006c56198f8f2f1
chris4474 <c237537@yahoo.fr>
2022-01-26 16:54:02 +0000

    create certs with year TTL

--- create-cert.sh
+++ create-cert.sh
@@ -7,14 +7,28 @@ fi
 node=$1.symphorines.home
 tmpdir=$(mktemp -d)
 
-vault write -format=json intca/issue/symphorines common_name=${node} alt_names=${node} >${tmpdir}/${node}.json
+ttl_1day=$(( 60 * 60 * 24 ))
+ttl_1year=$(( ttl_1day * 365 ))
+
+vault write -format=json intca/issue/symphorines common_name=${node} ttl=$ttl_1year alt_names=${node} >${tmpdir}/${node}.json
 
 cat ${tmpdir}/${node}.json | jq -r .data.private_key  >${tmpdir}/key.pem
+cat ${tmpdir}/${node}.json | jq -r .data.certificate  >${tmpdir}/cert.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0]  >${tmpdir}/intca.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[1]  >${tmpdir}/ca.crt
+
 cat ${tmpdir}/${node}.json | jq -r .data.certificate  >${tmpdir}/cert-plus-intca.pem
 cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0] >>${tmpdir}/cert-plus-intca.pem
-cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[1]  >${tmpdir}/ca.crt
-cat ${tmpdir}/${node}.json | jq -r .data.certificate  >${tmpdir}/cert.pem
 
-tar -cvf $node-bundle.tar --directory ${tmpdir} --remove-files .
+
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0]  >${tmpdir}/cachain.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[1] >>${tmpdir}/cachain.pem
+
+cat ${tmpdir}/${node}.json | jq -r .data.private_key  >${tmpdir}/key.pem
+cat ${tmpdir}/${node}.json | jq -r .data.certificate >>${tmpdir}/fullchain.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[0] >>${tmpdir}/fullchain.pem
+cat ${tmpdir}/${node}.json | jq -r .data.ca_chain[1] >>${tmpdir}/fullchain.pem
+
+tar -cvf ./certs/$node-bundle.tar --directory ${tmpdir} --remove-files .
 
 
